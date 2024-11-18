bdd7f865c9138d1b4990bd8cf4f3c89b1bc92811
MateusKSC <mateusscheuermann@gmail.com>
2024-11-18 17:36:42 -0300

    End of unit test dev

--- SalesRecordParserApi/Controllers/SaleStatisticsController.cs
+++ SalesRecordParserApi/Controllers/SaleStatisticsController.cs
@@ -1,5 +1,4 @@
 using Microsoft.AspNetCore.Mvc;
-using SalesRecordParserApi.Models;
 using SalesRecordParserApi.Services;
 
 namespace SalesRecordParserApi.Controllers
@@ -8,18 +7,10 @@ namespace SalesRecordParserApi.Controllers
     [Route("[controller]")]
     public class SaleStatisticsController : ControllerBase
     {
-        private SaleStatisticsService saleStatisticsService = new SaleStatisticsService();
-
-        private readonly ILogger<SaleStatisticsController> _logger;
-
-        public SaleStatisticsController(ILogger<SaleStatisticsController> logger)
-        {
-            _logger = logger;
-        }
-
+        SaleStatisticsService saleStatisticsService = new SaleStatisticsServiceImpl();
 
         [HttpGet(Name = "GetSaleStatistics")]
-        public IActionResult GetMedianUnitCost()
+        public IActionResult GetSaleStatistics()
         {
             return Ok(saleStatisticsService.GetSaleStatistics());
         }
